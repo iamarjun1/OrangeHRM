68d38615387a2f4dc3b42d558a2efb6037e2b5e2
iamar <iamar@192.168.1.39>
2024-02-13 21:27:07 +0530

    commit

--- src/test/java/new1/new1/Login.java
+++ src/test/java/new1/new1/Login.java
@@ -6,32 +6,43 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Login {
 
-	
-	public static void main(String[] args) {
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20)) ;
-		
-		driver.findElement(By.xpath("//input[@name='username']")).sendKeys("Admin");
-		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin123");
-		driver.findElement(By.xpath("//button[@type='submit']")).click();
-		
-		/*
-		 * List<WebElement> huhu = driver.findElements(By.tagName("a")); int count =
-		 * huhu.size(); System.out.println(count); for (int i=1; i<count;i++)
-		 * System.out.println(huhu.get(i).getAttribute("href"));
-		 */
-		
-		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewAdminModule");
-		driver.findElement(By.xpath("(//input[contains(@class,'oxd-input oxd-input--active')])[2]")).sendKeys("Arjun001");
-		driver.findElement(By.xpath("//div[@class='oxd-select-text-input' and contains(text(),'Admin')]")).click();
-		
-	}
-	
-	
-	
+    public static WebDriver driver = new ChromeDriver();
+    public static WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+    
+    public static void main(String[] args) throws Exception {
+    
+        driver.manage().window().maximize();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20)) ;
+        
+        driver.findElement(By.xpath("//input[@name='username']")).sendKeys("Admin");
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin123");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        
+        
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewAdminModule");
+        driver.findElement(By.xpath("(//input[contains(@class,'oxd-input oxd-input--active')])[2]")).sendKeys("Arjun001");
+        driver.findElement(By.xpath("(//div[@class='oxd-select-text-input'])[1]")).click();
+        Thread.sleep(500);
+        driver.findElement(By.xpath("(//div[@class='oxd-select-option' ])[2]")).click();
+        Thread.sleep(500);
+        driver.findElement(By.xpath("//input[@placeholder='Type for hints...']")).sendKeys("sha");
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//span[text()='fgasgrndahfsnhrfw  snnjtfc ']")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("(//div[@class='oxd-select-text-input'])[2]")).click();
+        Thread.sleep(500);
+        driver.findElement(By.xpath("(//div[@class='oxd-select-option'])[2]")).click();
+        Thread.sleep(500);
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        
+    }
+    
+    
+    
 }
